38a761231ae9ffdbf031a9187d39012c44e32131
minhduc152001 <mduc152001@gmail.com>
2024-05-29 21:21:10 +0700

    remove necessary part

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "tiktok-live-chat",
       script: "src/index.js",
-      out_file: "./logs/combined.log", // Custom path for standard output log
-      error_file: "./logs/combined.log", // Custom path for error log
+      out_file: "./logs/2024-05-29.log", // Custom path for standard output log
+      error_file: "./logs/2024-05-29.log", // Custom path for error log
       log_date_format: "YYYY-MM-DD HH:mm:ss", // Timestamp format for each log entry
     },
   ],
